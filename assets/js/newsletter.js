34191962228fce939c0cc9c48e8959abd07d8fa5
Youcef <youcefcopy@gmail.com>
2025-11-26 19:25:31 +0100

    e.target does not work with icons

--- assets/js/newsletter.js
+++ assets/js/newsletter.js
@@ -3,8 +3,8 @@
 const newsLetterBtn = document.getElementById('newsLetterBtn');
 console.log(newsLetterBtn);
 
-newsLetterBtn.addEventListener('click' , function() {
-    console.log(this);
-    this.classList.toggle('fa-solid');
-    this.classList.toggle('fa-angle-down');
+newsLetterBtn.addEventListener('click' , (e) => {
+    e.target.classList.toggle('a-solid');
+    e.target.classList.toggle('fa-angle-down');
+    console.log(e.target);
 })
