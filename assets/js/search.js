81ce4dbae1046eee6897cac919624f1e9857da7a
Youcef <youcefcopy@gmail.com>
2025-11-28 22:21:16 +0100

    close search bar & nav bar hidden

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,13 +1,20 @@
 // searchIcon
-console.log('searched loaded');
+console.log("searched loaded");
 console.log(newsLetterContainer);
 const searchIcon = document.getElementById("searchIcon");
 const searchSideBar = document.getElementById("searchSideBar");
 
-
 searchIcon.addEventListener("click", () => {
   console.log(searchIcon);
-  console.log('clicked');
+  console.log("clicked");
   searchSideBar.classList.toggle("hidden");
-  newsLetterBtn.classList.toggle('hidden');
+  newsLetterBtn.classList.toggle("hidden");
+});
+
+const closeSearchBar = document.getElementById("closeSearchBar");
+closeSearchBar.addEventListener("click", () => {
+  searchSideBar.classList.toggle("hidden");
+  newsLetterBtn.classList.toggle("hidden");
+
+  console.log("icon clicked!!!");
 });
