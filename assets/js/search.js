1ca88bdc3420a7cc4e3a47c74c905a5828090ea3
Youcef <youcefcopy@gmail.com>
2025-11-28 20:53:30 +0100

    js issue in hiding the newletter btn

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,7 +1,15 @@
 // searchIcon
 const searchIcon = document.getElementById("searchIcon");
 const searchSideBar = document.getElementById('searchSideBar');
+const newsLetterBtn = document.querySelector('#newsLetterBtn');
+
 searchIcon.addEventListener("click", () =>
 {     console.log(searchIcon)
-    searchSideBar.classList.toggle('hidden')}
+    searchSideBar.classList.toggle('hidden')
+    if (newsLetterBtn.contains('hidden')) {
+        newsLetterBtn.classList.remove('hidden')
+    } else {
+        newsLetterBtn.classList.add('hidden')
+    }
+}
 );
