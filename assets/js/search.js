7fa5d60716de0bf784b71cb4ff807dd6019e64ad
Youcef <youcefcopy@gmail.com>
2025-11-28 11:51:00 +0100

    hideen side bar works

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,3 +1,7 @@
 // searchIcon
-const searchIcon = document.getElementById('searchIcon');
-console.log(searchIcon);
+const searchIcon = document.getElementById("searchIcon");
+const searchSideBar = document.getElementById('searchSideBar');
+searchIcon.addEventListener("click", () =>
+{     console.log(searchIcon)
+    searchSideBar.classList.toggle('hidden')}
+);
