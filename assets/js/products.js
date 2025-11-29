da5c944a12deaf5c1d91d353edca796b6af58704
Youcef <youcefcopy@gmail.com>
2025-11-29 21:23:22 +0100

    change title content is functional

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,10 +1,28 @@
 // products logic
 const btnsContainer = document.querySelector('.btns');
+const viewAllBtn = document.getElementById('viewAllBtn');
 const clothesBtn = document.getElementById('clothesBtn');
-window.alert(btnsContainer);
+const shoesBtn = document.getElementById('shoesBtn');
+const AccBtn = document.getElementById('AccBtn');
+
+// window.alert(btnsContainer);
+const title = document.querySelector('.title');
 
 btnsContainer.addEventListener('click', (e) => {
-    if (e.target === clothesBtn) {
-        console.log("yes we clicking");
+    if (e.target === viewAllBtn) {
+        title.textContent = 'View All';
+    } else if (e.target === clothesBtn) {
+        title.textContent = 'Clothes';
+    } else if (e.target === shoesBtn) {
+        title.textContent = 'Shoes';
+    }  else if (e.target === AccBtn) {
+        title.textContent = 'Accessories';
     }
+    console.log(e.target);
 })
+
+
+function changeTitle(e) {
+    
+}
+
