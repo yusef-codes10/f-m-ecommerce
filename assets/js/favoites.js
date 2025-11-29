d2fc5fe2bdb67afb290247b399adea0f5124babc
Youcef <youcefcopy@gmail.com>
2025-11-29 19:38:31 +0100

    we have to import it in the module duh

--- assets/js/favoites.js
+++ assets/js/favoites.js
@@ -1,3 +1,10 @@
 // this is the functionality of adding products to fav
 // using an empty array & local storage
 let favoriteProducts = [];
+
+const favoriteIcon = document.getElementById('favoriteIcon');
+console.log('favorite icon is woooorking duh');
+favoriteIcon.addEventListener('click', () => {
+    window.alert('fav clicked');
+    console.log('I lciked lit looooooooooooooool');
+})
