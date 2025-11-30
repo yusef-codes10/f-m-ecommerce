3e54d00a3d705bd40c63f96a71ecabfbb147ef5c
Youcef <youcefcopy@gmail.com>
2025-11-30 16:55:03 +0100

    change color function passed as an arg

--- assets/js/btnBackground.js
+++ assets/js/btnBackground.js
@@ -4,3 +4,8 @@ const container = document.querySelectorAll("button");
 container.forEach(element => {
     console.log(element.textContent);
 });
+
+container.forEach(changeColor);
+function changeColor(e) {
+    e.style.color = 'red';
+}
