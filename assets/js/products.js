7f290dcf27963e85a8c82e8fcd2bd7e09aca36ab
Youcef <youcefcopy@gmail.com>
2025-11-30 15:49:20 +0100

    putting everything in one fucntion changeTitle() for reusability & stuff

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,31 +1,29 @@
 // products logic
-const btnsContainer = document.querySelector('.btns');
-const viewAllBtn = document.getElementById('viewAllBtn');
-const clothesBtn = document.getElementById('clothesBtn');
-const shoesBtn = document.getElementById('shoesBtn');
-const AccBtn = document.getElementById('AccBtn');
+const btnsContainer = document.querySelector(".btns");
+const viewAllBtn = document.getElementById("viewAllBtn");
+const clothesBtn = document.getElementById("clothesBtn");
+const shoesBtn = document.getElementById("shoesBtn");
+const AccBtn = document.getElementById("AccBtn");
 
 // window.alert(btnsContainer);
-const title = document.querySelector('.title');
+const title = document.querySelector(".title");
 
-btnsContainer.addEventListener('click', (e) => {
-    if (e.target === viewAllBtn) {
-        title.textContent = 'View All';
-    } else if (e.target === clothesBtn) {
-        title.textContent = 'Clothes';
-    } else if (e.target === shoesBtn) {
-        title.textContent = 'Shoes';
-    }  else if (e.target === AccBtn) {
-        title.textContent = 'Accessories';
-    }
-    console.log(e.target);
-})
-
+btnsContainer.addEventListener("click", (e) => {
+  changeTitle(e);
+  console.log(e.target);
+});
 
 function changeTitle(e) {
-    
+  if (e.target === viewAllBtn) {
+    title.textContent = "View All";
+  } else if (e.target === clothesBtn) {
+    title.textContent = "Clothes";
+  } else if (e.target === shoesBtn) {
+    title.textContent = "Shoes";
+  } else if (e.target === AccBtn) {
+    title.textContent = "Accessories";
+  }
 }
-
 
 // TODO put the functionality in a sigle func
 // TODO reduce code by loop or something
